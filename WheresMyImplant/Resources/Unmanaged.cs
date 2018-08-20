c98bc28b442d706dee434a56f0ed8268d50781ed
Alexander <Alexander@gegitech.net>
2018-08-20 12:55:44 -0500

    Adding Missing native methods

--- WheresMyImplant/Resources/Unmanaged.cs
+++ WheresMyImplant/Resources/Unmanaged.cs
@@ -54,6 +54,45 @@ namespace WheresMyImplant
         [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
         public static extern Boolean ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer, UInt32 nSize, ref UInt32 lpNumberOfBytesRead);
 
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern Boolean OpenProcessToken(IntPtr hProcess, UInt32 dwDesiredAccess, out IntPtr hToken);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern Boolean CloseHandle(IntPtr hProcess);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern Boolean DuplicateTokenEx(IntPtr hExistingToken, UInt32 dwDesiredAccess, IntPtr lpTokenAttributes, Enums._SECURITY_IMPERSONATION_LEVEL ImpersonationLevel, Enums.TOKEN_TYPE TokenType, out IntPtr phNewToken);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern Boolean ImpersonateLoggedOnUser(IntPtr hToken);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern Boolean CreateProcessWithTokenW(IntPtr hToken, Enums.LOGON_FLAGS dwLogonFlags, IntPtr lpApplicationName, IntPtr lpCommandLine, Enums.CREATION_FLAGS dwCreationFlags, IntPtr lpEnvironment, IntPtr lpCurrentDirectory, ref Structs._STARTUPINFO lpStartupInfo, out Structs._PROCESS_INFORMATION lpProcessInfo);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern Boolean CreateProcessWithLogonW(String userName, String domain, String password, Int32 logonFlags, String applicationName, String commandLine, int creationFlags, IntPtr environment, String currentDirectory, ref Structs._STARTUPINFO startupInfo, out Structs._PROCESS_INFORMATION processInformation);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern Boolean OpenThreadToken(IntPtr ThreadHandle, UInt32 DesiredAccess, Boolean OpenAsSelf, ref IntPtr TokenHandle);
+        
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetCurrentThread();
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern Boolean ImpersonateSelf(Enums.SECURITY_IMPERSONATION_LEVEL ImpersonationLevel);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern Boolean LookupPrivilegeName(String lpSystemName, IntPtr lpLuid, System.Text.StringBuilder lpName, ref Int32 cchName);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern Boolean LookupPrivilegeValue(String lpSystemName, String lpName, ref Structs._LUID luid);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern Boolean AdjustTokenPrivileges(IntPtr TokenHandle, Boolean DisableAllPrivileges, ref Structs._TOKEN_PRIVILEGES NewState, UInt32 BufferLengthInBytes, ref Structs._TOKEN_PRIVILEGES PreviousState, out UInt32 ReturnLengthInBytes);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern Boolean GetTokenInformation(IntPtr TokenHandle, Enums._TOKEN_INFORMATION_CLASS TokenInformationClass, IntPtr TokenInformation, UInt32 TokenInformationLength, out UInt32 ReturnLength);
+
         ////////////////////////////////////////////////////////////////////////////////
         public const UInt32 PROCESS_CREATE_THREAD = 0x0002;
         public const UInt32 PROCESS_QUERY_INFORMATION = 0x0400;
